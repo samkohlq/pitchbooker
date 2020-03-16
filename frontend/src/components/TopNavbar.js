1ed98dd21bd51b243ae91fcda29d459c06f3c2a7
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-16 17:44:20 +0800

    implemented firebaseui email and password authentication

--- frontend/src/components/TopNavbar.js
+++ frontend/src/components/TopNavbar.js
@@ -1,20 +1,48 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import { NavDropdown } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import firebase from "../firebase";
 
 // In this component will contain both login and create account component, and the state will contain the organization name if logged in
+var firebaseui = require("firebaseui");
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      return true;
+    }
+  },
+  signInFlow: "redirect",
+  signInSuccessUrl: "/",
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
+};
+
+const createAccount = () => {
+  ui.start("#firebaseui-auth-container", uiConfig);
+};
 
 export default class TopNavbar extends React.Component {
   render() {
     return (
       <div>
-        <Navbar fixed="top" bg="light" expand="lg">
-          <Navbar.Brand>Pitch Booker</Navbar.Brand>
-          <Navbar>
-            <Button variant="primary">Create Account</Button>
-            <Button variant="primary">Login</Button>
+        <div>
+          <Navbar
+            className="justify-content-between"
+            fixed="top"
+            bg="light"
+            expand="lg"
+          >
+            <Navbar.Brand>Pitch Booker</Navbar.Brand>
+            <NavDropdown title="Field Providers">
+              <NavDropdown.Item onClick={() => createAccount()}>
+                Create account
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+            </NavDropdown>
           </Navbar>
-        </Navbar>
+        </div>
+        <div id="firebaseui-auth-container"></div>
       </div>
     );
   }
