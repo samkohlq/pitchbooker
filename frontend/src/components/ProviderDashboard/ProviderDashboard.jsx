6e2ebe3a2954db6c00740094b5ab15a3a3e03efe
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-03-17 17:28:58 +0800

    move provider components into ProviderDashboard folder, add comments to AddPitch.jsx

---
+++ frontend/src/components/ProviderDashboard/ProviderDashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddPitch from "./AddPitch";
+
+class ProviderDashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <AddPitch />
+      </div>
+    );
+  }
+}
+
+export default ProviderDashboard;
