05882167823823c860d68dce7c6d4dafbfd82d51
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-17 19:56:56 +0800

    moved login component and function to seperate component

--- frontend/src/components/TopNavbar.js
+++ frontend/src/components/TopNavbar.js
@@ -1,45 +1,22 @@
 import React from "react";
 import { NavDropdown } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
-import firebase from "../firebase";
-
-var firebaseui = require("firebaseui");
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-var anonymousUser = firebase.auth().currentUser;
-
-const uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      return true;
-    }
-  },
-  autoUpgradeAnonymousUsers: true,
-  signInSuccessUrl: "/",
-  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
-};
-const login = () => {
-  ui.start("#firebaseui-auth-container", uiConfig);
-};
+import { loginFunction } from "./Login";
+import "./TopNavbar.css";
 
 export default class TopNavbar extends React.Component {
   // TODO(Perry): Make a onAuthStateChange in ComponentDidMount to ask for organisation name and phone number to send to the backend
   render() {
     return (
-      <div>
-        <div>
-          <Navbar
-            className="justify-content-between"
-            fixed="top"
-            bg="light"
-            expand="lg"
-          >
-            <Navbar.Brand>Pitch Booker</Navbar.Brand>
-            <NavDropdown title="Field Providers">
-              <NavDropdown.Item onClick={() => login()}>Login</NavDropdown.Item>
-            </NavDropdown>
-          </Navbar>
-        </div>
-        <div id="firebaseui-auth-container"></div>
+      <div className="top-navbar-container">
+        <Navbar className="justify-content-between" bg="light" expand="lg">
+          <Navbar.Brand>Pitch Booker</Navbar.Brand>
+          <NavDropdown title="Field Providers">
+            <NavDropdown.Item onClick={() => loginFunction()}>
+              Login
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Navbar>
       </div>
     );
   }
