e08a369cfe6d2a2358bcfc1108f46b0ac9d019e8
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-03-17 17:33:44 +0800

    create and add Pitch, PitchesList components

--- frontend/src/components/ProviderDashboard/ProviderDashboard.jsx
+++ frontend/src/components/ProviderDashboard/ProviderDashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AddPitch from "./AddPitch";
+import PitchesList from "./PitchesList";
 
 class ProviderDashboard extends React.Component {
   render() {
     return (
       <div>
         <AddPitch />
+        <PitchesList />
       </div>
     );
   }
