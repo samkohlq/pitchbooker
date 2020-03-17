d3266a4c0af6911c322cc9be230fdf909c6aebf1
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-17 19:56:11 +0800

    added new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import FieldForm from "./components/FieldForm";
+import Login from "./components/Login";
 import TopNavbar from "./components/TopNavbar";
 
-function App() {
-  return (
-    <div>
-      <TopNavbar />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TopNavbar />
+        <Login />
+        <FieldForm />
+      </div>
+    );
+  }
 }
 
 export default App;
