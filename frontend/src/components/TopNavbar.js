4fa6254c24ae9927e0227f4d225e673b9a80b631
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-18 15:06:13 +0800

    modified navbar to only show login button if user is not logged in

--- frontend/src/components/TopNavbar.js
+++ frontend/src/components/TopNavbar.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { NavDropdown } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import firebase from "../firebase";
 import { loginFunction } from "./Login";
 
 export default class TopNavbar extends React.Component {
-  // TODO(Perry): Make a onAuthStateChange in ComponentDidMount to ask for organisation name and phone number to send to the backend
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
   render() {
-    return (
+    const providerLogin = this.state.loggedIn ? (
+      <div className="top-navbar-container">
+        <Navbar className="justify-content-between" bg="light" expand="lg">
+          <Navbar.Brand>Pitch Booker</Navbar.Brand>
+        </Navbar>
+      </div>
+    ) : (
       <div className="top-navbar-container">
         <Navbar className="justify-content-between" bg="light" expand="lg">
           <Navbar.Brand>Pitch Booker</Navbar.Brand>
@@ -18,5 +39,6 @@ export default class TopNavbar extends React.Component {
         </Navbar>
       </div>
     );
+    return providerLogin;
   }
 }
