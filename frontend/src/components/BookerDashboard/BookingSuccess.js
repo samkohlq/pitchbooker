9cdc4baac9ca737e01c210dabbd0c60f5a1327af
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-24 17:39:56 +0800

    changed date render to be user friendly, rendered total cost

--- frontend/src/components/BookerDashboard/BookingSuccess.js
+++ frontend/src/components/BookerDashboard/BookingSuccess.js
@@ -4,25 +4,29 @@ export default class BookingSuccess extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bookingStartDate: props.location.state.bookingStartDate,
-      bookingEndDate: props.location.state.bookingEndDate,
+      bookingStartDate: new Date(props.location.state.bookingStartDate),
+      bookingEndDate: new Date(props.location.state.bookingEndDate),
       pitchAddress: props.location.state.pitchAddress,
-      pitchName: props.location.state.pitchName
+      pitchName: props.location.state.pitchName,
+      totalPrice: props.location.state.totalPrice
     };
   }
   //Todo(Perry): Calculate total and display it in confirmation page. Make this card centered.
   render() {
-    console.log(this.state.bookingStartDate);
     return (
       <div>
         <Card style={{ width: "18rem" }}>
           <Card.Body>
             <Card.Title>Your booking has been confirmed!</Card.Title>
-            <Card.Text>From: {this.state.bookingStartDate}</Card.Text>
-            <Card.Text>To: {this.state.bookingEndDate}</Card.Text>
+            <Card.Text>
+              From: {this.state.bookingStartDate.toLocaleString()}
+            </Card.Text>
+            <Card.Text>
+              To: {this.state.bookingEndDate.toLocaleString()}
+            </Card.Text>
             <Card.Text>Address: {this.state.pitchAddress}</Card.Text>
             <Card.Text>Venue: {this.state.pitchName}</Card.Text>
-            <Card.Text>Total:</Card.Text>
+            <Card.Text>Total:{this.state.totalPrice} </Card.Text>
           </Card.Body>
         </Card>
       </div>
