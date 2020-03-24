4ecb5bc41ccbc509f202169f20f50eb64691a5af
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-03-24 18:01:48 +0800

    use ES6 syntax in emailController for consistency

--- backend/src/controllers/emailController.js
+++ backend/src/controllers/emailController.js
@@ -2,13 +2,15 @@ const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export const sendBookingConfirmationEmail = (req, res) => {
+export const sendBookingConfirmationEmail = async (req, res) => {
   const msg = {
     to: req.body.bookerEmail,
     from: "samkohlq@gmail.com",
-    subject: "We have confirmed your booking",
+    subject: "We have confirmed your pitch booking",
     text: `Hello ${req.body.bookerName}! We have confirmed your booking from ${req.body.bookingStartDateTime} to ${req.body.bookingEndDateTime}`
   };
-  sgMail.send(msg).then(() => {}, console.error);
+  await sgMail.send(msg).catch(error => {
+    console.log(error);
+  });
   res.send(msg);
 };
