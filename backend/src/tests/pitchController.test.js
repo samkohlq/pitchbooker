a055b22e83f1a3c2d0d9aaf0aa371ac587f4f4a6
lohengliang <60084141+lohengliang@users.noreply.github.com>
2020-03-27 22:13:50 +0800

    Adjusted tests to fit timezone diff.

--- backend/src/tests/pitchController.test.js
+++ backend/src/tests/pitchController.test.js
@@ -127,8 +127,8 @@ test("retrieve pitches API to return pitch details if query has currentUserUid, 
   expect(retrievePitchResponse.body[0].pricePerHour).toBe(123);
   expect(retrievePitchResponse.body[0].address).toBe("Test Pitch Road");
   expect(retrievePitchResponse.body[0].maxNumPlayersPerSide).toBe(9);
-  const startDateTime = new Date(Date.now() + 100 * 60 * 60);
-  const endDateTime = new Date(Date.now() + 100 * 60 * 60);
+  const startDateTime = new Date(Date.now() + 1000 * 60 * 60);
+  const endDateTime = new Date(Date.now() + 1000 * 60 * 60);
   let maxNumPlayersPerSide = 8;
   const retrievePitchResponseWithoutUserUidDiffMaxNumPlayersPerSide = await request(
     app
